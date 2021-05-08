6579fdbc47df01ac51ac0acbc0d23767d430e4f5
Mert <mert.ejd@gmail.com>
2021-05-08 16:12:40 +0300

    Initial Commit

---
+++ TrCellProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrCellProject
+//
+//  Created by Mert Ejder on 8.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
